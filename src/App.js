f98cdfc1839b7b7f2962e07385b6f1c75b1efaaf
snrelghgub <snrelghmail@gmail.com>
2019-08-05 01:58:05 +1000

    used browser API to fetch quotes & update quotesList's initial empty state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import './App.css';
 import Button from './components/Button.js'; 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quotesList: [],
+      quotePointer: null, 
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+    .then(rawdata => rawdata.json())
+    .then(quotesList => this.setState({
+      quotesList: {quotesList}
+    }));
+  }
+
   getNewQuote(){
     console.log("working"); 
   }
 
   render() {
+    console.log(this.state.quotesList);
     return (
       <div className="App" id="quote-box">
         <Button buttonText="New quote" fetchNewQuote={this.getNewQuote}/>
