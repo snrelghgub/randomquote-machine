9207481921c0ee7332caf58d3279b0c2bc62f052
snrelghgub <snrelghmail@gmail.com>
2019-08-05 00:49:12 +1000

    added pre-functional click handler to App container component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button.js'; 
 
-function App() {
-  return (
-    <div className="App" id="quote-box">
-     <Button buttonText="New quote"/>
-    </div>
-  );
+class App extends React.Component {
+  getNewQuote(){
+    console.log("working"); 
+  }
+
+  render() {
+    return (
+      <div className="App" id="quote-box">
+        <Button buttonText="New quote" fetchNewQuote={this.getNewQuote}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
